6138d08779cad4e235c0d1d8265fdbc9aaabbefb
Shadow-Lord <dhilan.rohatgi@gmail.com>
2025-12-01 13:25:53 -0600

    Setup workspace and started making model and analyzing data

--- model.py
+++ model.py
@@ -1,2 +1,37 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report, confusion_matrix
+from lazypredict.Supervised import LazyClassifier
+
+df = pd.read_csv("dataset\synthetic_medical_symptoms_dataset.csv")
+df.head()
+print(df["diagnosis"].unique())
+
+df = df.dropna()
+
+plt.figure(figsize=(10, 8))
+sns.heatmap(df.corr(numeric_only=True), annot=False, cmap="coolwarm")
+plt.show()
+
+X = df.drop("diagnosis", axis=1)
+y = df["diagnosis"]
+
+label_encoders = {}
+for col in X.select_dtypes(include=["object"]).columns:
+    le = LabelEncoder()
+    X[col] = le.fit_transform(X[col])
+    label_encoders[col] = le
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+clf = LazyClassifier(verbose=0, ignore_warnings=True, custom_metric=None)
+models, predictions = clf.fit(X_train, X_test, y_train, y_test)
+
+print(models)
